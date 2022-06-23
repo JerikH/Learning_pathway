be44f9d1427e9984067c7e6398a6cb07026f25e1
Jerik David Hincapie Bedoya <jerik.hincapie@utp.edu.co>
2022-06-22 19:19:30 -0500

    Add Operadores in Python path

---
+++ Python/1.Operadores/Ejercicio1.py
@@ -0,0 +1,17 @@
+# Ejecución directa sin variables
+# print(((3 + 2) / (2 * 5)) ** 2)
+
+num1 = 3
+num2 = 2
+num3 = 5
+'''
+# ejecución por partes con variables
+suma = num1 + num2
+multiplicacion = num2 * num3
+division = suma / multiplicacion
+potencia = division ** 2
+
+print(potencia)
+'''
+# ejecución directa con variables
+print(((num1 + num2) / (num2 * num3)) ** 2)
---
+++ Python/1.Operadores/Ejercicio2.py
@@ -0,0 +1,8 @@
+W_payaso = 112
+W_muneca = 75
+
+# Calcular peso total de 23 payasos y 54 muñecas
+peso_total = W_payaso * 23 + W_muneca * 54
+
+# Imprimir peso total en gramos
+print(peso_total, "gramos")
