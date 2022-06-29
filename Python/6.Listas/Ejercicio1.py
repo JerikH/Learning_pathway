4cec610d990790206623e94451c36b3f8a180334
Jerik David Hincapie Bedoya <jerik.hincapie@utp.edu.co>
2022-06-28 21:44:39 -0500

    Listas path

---
+++ Python/6.Listas/Ejercicio1.py
@@ -0,0 +1,5 @@
+lista = [20, 50, "Curso", 'Python', 3.14]
+print("La lista original es: ", lista)
+lista[0] = input("Introduce un nuevo elemento para el primer lugar de la lista: ")
+lista[1] = input("Introduce un nuevo elemento para el segundo lugar de la lista: ")
+print("La lista modificada es: ", lista)
---
+++ Python/6.Listas/Ejercicio2.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("La lista original es: ", numbers)
+numbers[4] *= 2
+numbers[7] *= 2
+numbers[9] *= 2
+print("La lista modificada es: ", numbers)
