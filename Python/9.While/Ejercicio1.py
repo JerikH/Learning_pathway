9dc5998c08c94c0a2180d8bfc0fda5a01a0eee10
Jerik David Hincapie Bedoya <jerik.hincapie@utp.edu.co>
2022-07-06 23:58:23 -0500

    while

---
+++ Python/9.While/Ejercicio1.py
@@ -0,0 +1,6 @@
+num = int(input("Ingrese un número entero: "))
+i = 0
+print("\tLa tabla del ", num)
+while i < 11:
+    print("\t", num, "x", i, "=", num * i)
+    i += 1
---
+++ Python/9.While/Ejercicio2.py
@@ -0,0 +1,5 @@
+edad = int(input("Ingrese su edad: "))
+i = 0
+while i < edad:
+    i += 1
+    print("\t", i)
